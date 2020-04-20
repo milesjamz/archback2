e56d4fee68cb016827d18369dcc6a7634e88730b
Miles Marillo <mjm03@hampshire.edu>
2020-04-20 15:09:51 -0400

    user serializers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,6 +7,16 @@
           render json: { error: 'failed to create user' }, status: :not_acceptable
         end
       end
+
+      def index
+        users = User.all
+        render json: users
+      end
+
+      def show
+        user = User.find(params[:id])
+        render json: user
+      end
      
       private
       def user_params

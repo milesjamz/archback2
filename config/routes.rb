d9f73291dc26f36f5890f67425c1af2d80ee26c5
Miles Marillo <mjm03@hampshire.edu>
2020-11-27 14:15:03 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@
         namespace :v1 do
           resources :users
           resources :allergens
+          resources :food_days
+          resources :meals
+          resources :symptoms
           post '/login', to: 'auth#create'
           get '/profile', to: 'users#profile'
           resources :days
